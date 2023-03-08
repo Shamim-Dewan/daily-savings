95e0cc46eff6fecee89286a8ea25497f6391c616
Shamim-Dewan <570iamkhan@gmail.com>
2023-03-08 17:46:52 +0600

    added conditions

--- js/script.js
+++ js/script.js
@@ -3,52 +3,60 @@ function calculateExpense(){
   const food = +document.getElementById("food").value;
   const rent = +document.getElementById("rent").value;
   const clothes = +document.getElementById("clothes").value;
-
+// cheacking income value
   if (
-    income < 0 ||
-    income == "" ||
-    food < 0 ||
-    food == "" ||
-    rent < 0 ||
-    rent == "" ||
-    clothes < 0 ||
-    clothes == ""
+    income < 1 ||
+    income == NaN ||
+    income == ""
   ) {
     alert("Inputs must be positive numbers");
-    return;
+    return document.getElementById("income").value = "";
   }
-  // calculate expense
-  const expense = food + rent + clothes;
-  // calculate balance
-  const balance = income - expense;
-  //   validate income
-  if (expense > income) {
-    alert("Expenses cannot be more than income");
-  } else {
-    // view total expense and balance
-    document.getElementById("total-expense").innerText = expense;
-    document.getElementById("balance").innerText = balance;
+  // checking food,rent and clothes value
+  if (
+    food < 1 || rent < 1 || clothes < 1) {
+    alert("Inputs must be positive numbers");
+    document.getElementById("food").value = "";
+    document.getElementById("rent").value = "";
+    document.getElementById("clothes").value = "";
+
   }
+  else{
+// calculate expense
+const expense = food + rent + clothes;
+// calculate balance
+const balance = income - expense;
+//   validate income
+if (expense > income) {
+  alert("Expenses cannot be more than income");
+} else {
+  // view total expense and balance
+  document.getElementById("total-expense").innerText = expense;
+  document.getElementById("balance").innerText = balance;
+}
+  }
+  
 };
 
 function calculateSavings(){
   // calculate saving amount
   const savePercentage = +document.getElementById("save").value;
 //   Validate saving percentage value
-  if (savePercentage < 0) {
+  if (savePercentage <= 0 || savePercentage > 100) {
     alert("Provide positive saving value");
+    document.getElementById("save").value = "";
   }
+  else{
 // calculat result of %
-  const income = +document.getElementById("income").value;
-  const balance = +document.getElementById("balance").innerText;
-  const savingAmount = (savePercentage / 100) * balance;
+const income = +document.getElementById("income").value;
+const balance = +document.getElementById("balance").innerText;
+const savingAmount = (savePercentage / 100) * balance;
 
-  // calculate remaining balance
-  const remainingBalance = balance - savingAmount;
-console.log(remainingBalance);
- 
-    // view saving amount and remaining balance
-    document.getElementById("saving-amount").innerText = savingAmount.toFixed(2);
-    document.getElementById("remaining-balance").innerText = remainingBalance;
-  
+// calculate remaining balance
+const remainingBalance = balance - savingAmount;
+
+  // view saving amount and remaining balance
+  document.getElementById("saving-amount").innerText = savingAmount.toFixed(2);
+  document.getElementById("remaining-balance").innerText = remainingBalance;
+  }
 };
